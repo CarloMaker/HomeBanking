b0fe06a5122e8a0826905c01d74d65392fdbb484
carlo carollo <carlo.carollo@ags.it.com>
2023-09-05 17:52:54 +0200

    first commit

---
+++ HbxClient/src/app/core/topbar/topbar.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-topbar',
+  templateUrl: './topbar.component.html',
+  styleUrls: ['./topbar.component.css']
+})
+export class TopbarComponent {
+
+  items: MenuItem[] | undefined;
+
+
+  logout(){
+
+  }
+
+}
